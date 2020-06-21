9d8aa0590fb1d552684869446143fc30487f6e3d
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-06-21 13:40:05 +0200

    ✨ Add support for separators in boxes

--- cli_box.py
+++ cli_box.py
@@ -23,6 +23,25 @@ It also works with ANSI escape codes:
 ┌────────────────────┐
 │ Some \033[33mcolored\033[0m text. │
 └────────────────────┘
+
+You can pass in an array of strings to get the separated with line separators:
+>>> print(box(["a multiline\\ntext\\non an area", "another \\033[34marea. \\033[0m\\nahaha"]))
+┌────────────────┐
+│  a multiline   │
+│      text      │
+│   on an area   │
+├────────────────┤
+│ another \033[34marea. \033[0m │
+│     ahaha      │
+└────────────────┘
+
+The characters for sides connected to separators are customizable:
+>>> print(box(["lorem", "ipsum"], connected_sides=("L", "R")))
+┌───────┐
+│ lorem │
+L───────R
+│ ipsum │
+└───────┘
 """
 from wcwidth import wcswidth
 from strip_ansi import strip_ansi
@@ -47,9 +66,10 @@ def align_text(text: str, width: int, align: str = "left") -> str:
 
 
 def box(
-    text: str,
+    text: Union[str, List[str]],
     corners: Tuple[str, str, str, str] = ("┌", "┐", "┘", "└"),
     sides: Union[Tuple[str, str, str, str], Tuple[str, str]] = ("│", "─"),
+    connected_sides: Tuple[str, str] = ("├", "┤"),
     background: str = " ",
     align: str = "center",
 ) -> str:
@@ -57,18 +77,33 @@ def box(
     if len(sides) == 2:
         sides = (sides[1], sides[0], sides[1], sides[0])
 
-    lines = text.splitlines()
+    if type(text) is str:
+        lines = text.splitlines()
+    else:
+        lines = []
+        for area in text:
+            for l in area.splitlines():
+                lines.append(l)
     text_width = max(strwidth(l) for l in lines)
     width = text_width + padding * 2
     res = [corners[0] + sides[0] * width + corners[1]]
-    for l in lines:
-        res.append(
-            sides[3]
-            + " " * (width - text_width - strwidth(sides[3]))
-            + align_text(l, text_width, align)
-            + " " * (width - text_width - strwidth(sides[1]))
-            + sides[1]
-        )
+    format_line = (
+        lambda l: sides[3]
+        + background * (width - text_width - strwidth(sides[3]))
+        + align_text(l, text_width, align)
+        + background * (width - text_width - strwidth(sides[1]))
+        + sides[1]
+    )
+    if type(text) is str:
+        for l in text.splitlines():
+            res.append(format_line(l))
+    else:
+        for i, area in enumerate(text):
+            for l in area.splitlines():
+                res.append(format_line(l))
+            # Don't add a separator if we're on the last area
+            if i != len(text) - 1:
+                res.append(connected_sides[0] + sides[2] * width + connected_sides[1])
     res.append(corners[3] + sides[2] * width + corners[2])
     return "\n".join(res)
 
